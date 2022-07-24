c6d62989d9c766c5eecfa4c7e652fa05c766deb9
buhe <bugu1986@126.com>
2022-07-24 20:38:51 +0800

    init

---
+++ README.md
@@ -0,0 +1,4 @@
+### Start
+```
+/Applications/Emacs.app/Contents/MacOS/Emacs -q -l ~/.emacs.d.bugu/init.el
+```
---
+++ init.el
@@ -0,0 +1,12 @@
+(global-linum-mode 1)
+;; hide toolbar menubar scrollbar
+(tool-bar-mode 0)
+(menu-bar-mode 0)
+(scroll-bar-mode 0)
+;; hightlight
+(global-hl-line-mode 1)
+;; show time
+(display-time-mode 1)
+(setq display-time-24hr-format t)
+(setq display-time-day-and-date t)
+(setq initial-frame-alist (quote ((fullscreen . maximized))))
