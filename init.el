09caf0507d755aee231fe0abc43ddf66ded6e252
buhe <bugu1986@126.com>
2022-07-24 21:24:21 +0800

    git-u:update

--- init.el
+++ init.el
@@ -10,3 +10,66 @@
 (setq display-time-24hr-format t)
 (setq display-time-day-and-date t)
 (setq initial-frame-alist (quote ((fullscreen . maximized))))
+
+(setq mac-option-key-is-meta t
+      x-select-enable-clipboard 't
+      ;; mac-command-key-is-meta nil
+      mac-command-modifier 'super
+      mac-option-modifier 'meta)
+
+;; 这两段一定要在 init.el 的最上方
+(require 'package)
+;; 初始化包管理器
+(package-initialize)
+
+;; 设置软件源
+;; 默认软件源里只有 ELPA，也就是 GNU Emacs 官方的软件源
+;; 我们引入以下几个最常用的软件源：
+
+;; MELPA：软件包比 ELPA 多（软件进入 MELPA 比 ELPA 手续更简单）、新
+;; （nightly 级别的更新速度，以时间作为版本号）
+(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t)
+;; 稳定版 MELPA （非 nightly，有版本号）
+(add-to-list 'package-archives '("melpa-stable" . "http://stable.melpa.org/packages/") t)
+;; org-mode 专用软件源。它几乎只服务于 org-plus-contrib 这一个包
+(add-to-list 'package-archives '("org" . "https://orgmode.org/elpa/") t)
+
+;; 以下用来 bootstrap use-package 自己。在上文设置好软件源后，
+
+;; 如果 use-package 没安装
+(unless (package-installed-p 'use-package)
+  ;; 更新本地缓存
+  (package-refresh-contents)
+  ;; 之后安装它。use-package 应该是你配置中唯一一个需要这样安装的包。
+  (package-install 'use-package))
+
+(require 'use-package)
+;; 让 use-package 永远按需安装软件包
+(setq use-package-always-ensure t)
+
+;; 之后就可以使用它了。
+;; 比如上文安装并 require better-defaults 的过程就可以简化为这一行：
+(use-package better-defaults)
+;; 1. 它会判断是否已安装。没有时才会更新 package 缓存并安装它
+;; 2. 它会自动 (require)
+;; 3. 它有很多配置项能让你控制每个环节，从而做到把和这个软件包有关的所
+;; 有配置写在一个闭包里。
+
+;; quelpa - For those packages which are not in MELPA
+(use-package quelpa
+  :config ; 在 (require) 之后需要执行的表达式
+  (use-package quelpa-use-package) ; 把 quelpa 嵌入 use-package 的宏扩展
+  (quelpa-use-package-activate-advice)) ; 启用这个 advice
+
+(use-package helm
+  ;; 等价于 (bind-key "M-x" #'helm-M-x)
+  :bind (("M-x" . helm-M-x)
+         ("C-x C-f" . helm-find-files))
+  :config
+  ;; 全局启用 Helm minor mode
+  (helm-mode 1))
+
+;; 好了。按个 M-x 试试
+
+
+
