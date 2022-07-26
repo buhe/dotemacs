877795c8f5d7eead6c56d100d3b37eac0651c88a
buhe <bugu1986@126.com>
2022-07-26 18:17:33 +0800

    git-u:update

--- init.el
+++ init.el
@@ -69,34 +69,7 @@
 
 ;; 好了。按个 M-x 试试
 
-(use-package company
-  ;; 等价于 (add-hook 'after-init-hook #'global-company-mode)
-  :hook (after-init . global-company-mode)
-  :config
-  ;; setq 可以像这样连着设置多个变量的值
-  (setq company-tooltip-align-annotations t ; 注释贴右侧对齐
-        company-tooltip-limit 20            ; 菜单里可选项数量
-        company-show-numbers t              ; 显示编号（然后可以用 M-数字 快速选定某一项）
-        company-idle-delay .2               ; 延时多少秒后弹出
-        company-minimum-prefix-length 1     ; 至少几个字符后开始补全
-        ))
 
-(use-package yasnippet
-  :ensure t
-  :hook
-  (prog-mode . yas-minor-mode)
-)
-
-(use-package yasnippet-snippets
-  :ensure t
-  :after yasnippet)
-
-
-(use-package flycheck
-  :init ;; 在 (require) 之前需要执行的
-  (setq flycheck-emacs-lisp-load-path 'inherit)
-  :config
-  (global-flycheck-mode))
 
 ;; ~/.emacs.d/init.el
 (use-package projectile
@@ -208,9 +181,6 @@
         ("C-x t C-t" . treemacs-find-file)
         ("C-x t M-t" . treemacs-find-tag)))
 
-(use-package treemacs-evil
-  :after (treemacs evil)
-  :ensure t)
 
 (use-package treemacs-projectile
   :after (treemacs projectile)
@@ -282,80 +252,8 @@
   (google-this-mode))
 
 
-(use-package lsp-mode
-  :ensure t
-  :init
-  ;; set prefix for lsp-command-keymap (few alternatives - "C-l", "C-c l")
-  (setq lsp-keymap-prefix "C-c l"
-	lsp-file-watch-threshold 500)
-  :hook 
-  (lsp-mode . lsp-enable-which-key-integration) ; which-key integration
-  :commands (lsp lsp-deferred)
-  :config
-    (setq lsp-completion-provider :none) ;; 阻止 lsp 重新设置 company-backend 而覆盖我们 yasnippet 的设置
-    (setq lsp-headerline-breadcrumb-enable t))
-
-(use-package lsp-ui
-  :ensure t
-  :config
-  (define-key lsp-ui-mode-map [remap xref-find-definitions] #'lsp-ui-peek-find-definitions)
-  (define-key lsp-ui-mode-map [remap xref-find-references] #'lsp-ui-peek-find-references)
-  (setq lsp-ui-doc-position 'top))
 
 
-(use-package dap-mode
-  :ensure t
-  :after hydra lsp-mode
-  :commands dap-debug
-  :custom
-  (dap-auto-configure-mode t)
-  :config
-  (dap-ui-mode 1)
-  :hydra
-  (hydra-dap-mode
-   (:color pink :hint nil :foreign-keys run)
-   "
-^Stepping^          ^Switch^                 ^Breakpoints^         ^Debug^                     ^Eval
-^^^^^^^^----------------------------------------------------------------------------------------------------------------
-_n_: Next           _ss_: Session            _bb_: Toggle          _dd_: Debug                 _ee_: Eval
-_i_: Step in        _st_: Thread             _bd_: Delete          _dr_: Debug recent          _er_: Eval region
-_o_: Step out       _sf_: Stack frame        _ba_: Add             _dl_: Debug last            _es_: Eval thing at point
-_c_: Continue       _su_: Up stack frame     _bc_: Set condition   _de_: Edit debug template   _ea_: Add expression.
-_r_: Restart frame  _sd_: Down stack frame   _bh_: Set hit count   _ds_: Debug restart
-_Q_: Disconnect     _sl_: List locals        _bl_: Set log message
-                  _sb_: List breakpoints
-                  _sS_: List sessions
-"
-   ("n" dap-next)
-   ("i" dap-step-in)
-   ("o" dap-step-out)
-   ("c" dap-continue)
-   ("r" dap-restart-frame)
-   ("ss" dap-switch-session)
-   ("st" dap-switch-thread)
-   ("sf" dap-switch-stack-frame)
-   ("su" dap-up-stack-frame)
-   ("sd" dap-down-stack-frame)
-   ("sl" dap-ui-locals)
-   ("sb" dap-ui-breakpoints)
-   ("sS" dap-ui-sessions)
-   ("bb" dap-breakpoint-toggle)
-   ("ba" dap-breakpoint-add)
-   ("bd" dap-breakpoint-delete)
-   ("bc" dap-breakpoint-condition)
-   ("bh" dap-breakpoint-hit-condition)
-   ("bl" dap-breakpoint-log-message)
-   ("dd" dap-debug)
-   ("dr" dap-debug-recent)
-   ("ds" dap-debug-restart)
-   ("dl" dap-debug-last)
-   ("de" dap-debug-edit-template)
-   ("ee" dap-eval)
-   ("ea" dap-ui-expressions-add)
-   ("er" dap-eval-region)
-   ("es" dap-eval-thing-at-point)
-   ("q" nil "quit" :color blue)
-   ("Q" dap-disconnect :color red)))
 
 
 (require 'hello)
